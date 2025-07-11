1dbf731d956e8583dfd2b9da752ce13908e23e62
Programmering <teknisk+programmering-GDC2025-GR5@game.unf.dk>
2025-07-11 17:01:50 +0200

    yayy

--- Assets/ShootingPoint.cs
+++ Assets/ShootingPoint.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShootingPoint : MonoBehaviour
 {
+
+    public float cooldown = 0.5f;
+    public float cooldownLeft;
+
+    
+    
+    public InputAction shoot;
 
     public GameObject bulletPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        shoot.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        cooldownLeft -= Time.deltaTime;
+        if (shoot.WasPressedThisFrame() && cooldownLeft <= 0)
+        {
+            GameObject tempBullet;
+        
+            tempBullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+            Vector3 rotationvector = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+            tempBullet.GetComponent<Rigidbody>().AddForce(rotationvector * 10000);
+            cooldownLeft = cooldown;
+            
+            
+            
+        }
+
+        
+
     }
 }
